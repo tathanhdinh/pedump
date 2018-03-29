1a51105a61efb424fb0869ae2f725d13178b4283
TA Thanh Dinh <tathanhdinh@gmail.com>
2018-03-29 16:22:32 +0200

    check PE type

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@ extern crate clap;
 extern crate goblin;
 #[macro_use] extern crate failure;
 
+use std::{io::Read};
+// use failure::{Error};
+
 // #[macro_use] extern crate failure_derive;
 
 
@@ -13,7 +16,18 @@ static APPLICATION_ABOUT: &'static str = "A PE dumper";
 static ARGUMENT_FILE: &'static str = "input file";
 
 fn main() {
-    // println!("Hello, world!");
+    match run() {
+        Ok(_) => {
+
+        },
+        
+        Err(err) => {
+            println!("Error: {}", err);
+        }
+    }
+}
+
+fn run() -> Result<(), failure::Error> {
     let matches = clap::App::new(APPLICATION_NAME)
         .version(APPLICATION_VERSION)
         .author(APPLICATION_AUTHOR)
@@ -24,8 +38,26 @@ fn main() {
         .get_matches();
 
     let input_file = matches.value_of(ARGUMENT_FILE).unwrap(); // should not panic
+    let file_mdt = std::fs::metadata(input_file)?;
+    if file_mdt.file_type().is_file() {
+        let mut fd = std::fs::File::open(input_file)?;
+        let mut buffer = Vec::new();
+        fd.read_to_end(&mut buffer)?;
+        let pe_object = goblin::pe::PE::parse(&buffer)?;
+        // println!("PE {:#?}", &pe_object);
+        dump_pe(&pe_object)?;
+    }
 
+    Ok(())
 }
 
-fn run() -> Result<(), failure::Error> {
+fn dump_pe(pe_object: &goblin::pe::PE) -> Result<(), failure::Error> {
+    // println!("{}", )
+    if pe_object.is_64 {
+        println!("{}", "PE32+");
+    }
+    else {
+        println!("{}", "PE32");
+    }
+    Ok(())
 }
